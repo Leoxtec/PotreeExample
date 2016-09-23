a9c7d84cbf7c2a3639329170000859a4416a28c8
Aldo Leonel Rodríguez Salazar <alrodriguez@ciatec.int>
2016-09-23 13:22:21 -0500

    PotreeExample

---
+++ resources/pointclouds/inegi/cloud.js
@@ -0,0 +1,28 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 740264,
+    "boundingBox": {
+        "lx": 535935.84,
+        "ly": 3284216.94,
+        "lz": 560.51,
+        "ux": 542866.0200000001,
+        "uy": 3291147.12,
+        "uz": 7490.690000000168
+    },
+    "tightBoundingBox": {
+        "lx": 535935.84,
+        "ly": 3284216.94,
+        "lz": 560.51,
+        "ux": 537624.17,
+        "uy": 3291147.12,
+        "uz": 1221.19
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 48.013694763183597,
+    "scale": 0.01,
+    "hierarchyStepSize": 5
+}
